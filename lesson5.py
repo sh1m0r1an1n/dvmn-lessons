ba933d7d401f02bcd2c04295de68c40a4de3441a
sh1m0r1an1n <akhmanaev2021@gmail.com>
2024-12-04 17:14:39 +0500

    Add files via upload

---
+++ lesson5.py
@@ -0,0 +1,92 @@
+import os
+import file_operations
+import random
+from faker import Faker
+
+os.makedirs('output/svg', mode=0o777, exist_ok=True)
+
+FAKE = Faker("ru_RU")
+
+SKILLS = [
+    'Стремительный прыжок',
+    'Электрический выстрел',
+    'Ледяной удар',
+    'Стремительный удар',
+    'Кислотный взгляд',
+    'Тайный побег',
+    'Ледяной выстрел',
+    'Огненный заряд'
+]
+
+RUNIC_ALPHABET = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+
+def main():
+    for n in range(10):
+
+        first_name = FAKE.first_name_male()
+        last_name = FAKE.last_name_male()
+        job = FAKE.job()
+        town = FAKE.city()
+        strength = random.randint(3, 18)
+        agility = random.randint(3, 18)
+        endurance = random.randint(3, 18)
+        intelligence = random.randint(3, 18)
+        luck = random.randint(3, 18)
+
+        random_skills = random.sample(SKILLS, 3)
+
+        runic_skills = []
+
+        for random_skill in random_skills:
+            for letter in random_skill:
+                random_skill = random_skill.replace(
+                    letter, RUNIC_ALPHABET[letter]
+                    )
+            runic_skills.append(random_skill)
+
+        context = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "town": town,
+            "job": job,
+            "strength": strength,
+            "agility": agility,
+            "endurance": endurance,
+            "intelligence": intelligence,
+            "luck": luck,
+            "skill_1": runic_skills[0],
+            "skill_2": runic_skills[1],
+            "skill_3": runic_skills[2],
+        }
+
+        file_operations.render_template(
+            "src/template.svg", "output/svg/result-{}.svg".format(n), context)
+
+
+if __name__ == '__main__':
+    main()
