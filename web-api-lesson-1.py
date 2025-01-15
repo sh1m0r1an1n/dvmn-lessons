25c1c5830d2e4a9d24bf24c923ee301d07607483
sh1m0r1an1n <akhmanaev2021@gmail.com>
2025-01-15 22:44:28 +0500

    Update web-api-lesson-1.py

--- web-api-lesson-1.py
+++ web-api-lesson-1.py
@@ -1,18 +1,17 @@
 import requests
-from urllib.parse import quote
 
 
 def main():
-    city = input('Введите местоположение: ')
-    url = f'https://wttr.in/{quote(city)}?nTqM&lang=ru'
-
-    try:
-        response = requests.get(url)
+    cities = ['Лондон', 'аэропорт Шереметьево', 'Череповец']
+    params = {
+        'nTqM': '',
+        'lang': 'ru'
+    }
+    for city in cities:
+        url = f'https://wttr.in/{city}'
+        response = requests.get(url, params=params)
         response.raise_for_status()
-        response.encoding = 'utf-8'
         print(response.text)
-    except requests.exceptions.RequestException as error:
-        print(f"Ошибка при запросе: {error}")
 
 
 if __name__ == '__main__':
