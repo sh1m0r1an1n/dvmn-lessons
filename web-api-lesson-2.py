f80ed53b7accb0d973be08073e28985395463cbe
sh1m0r1an1n <akhmanaev2021@gmail.com>
2025-01-20 15:20:28 +0500

    Update web-api-lesson-2.py

--- web-api-lesson-2.py
+++ web-api-lesson-2.py
@@ -40,48 +40,28 @@ def count_clicks(token: str, url: str) -> int:
     return stats[0]['views'] if stats else 0
 
 
-def validate_url(url: str) -> bool:
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return True
-    except requests.exceptions.HTTPError:
-        return False
-
-
 def is_shorten_link(url: str) -> bool:
-    return urlparse(url).netloc == 'vk.cc'
+    response = requests.get(url)
+    response.raise_for_status()
+    return urlparse(url).netloc == 'vk.cc' and urlparse(url).path.lstrip("/")
 
 
 def main():
     load_dotenv()
-    token = os.getenv('VK_TOKEN')
-
-    if not token:
-        print("Токен VK отсутствует. Проверьте файл .env.")
-        return
+    token = os.environ['VK_TOKEN']
 
     url = input('Введите ссылку: ')
 
-    if not validate_url(url):
-        print("Некорректный URL. Попробуйте снова.")
-        return
-
     try:
-        if not is_shorten_link(url):
-            short_link = shorten_link(token, url)
-            print('Сокращенная ссылка: ', short_link)
-        else:
-            short_link = url
-
-        parsed_url = urlparse(short_link)
-        short_url_key = parsed_url.path.lstrip("/")
-
         if is_shorten_link(url):
+            short_url_key = urlparse(url).path.lstrip("/")
             click_count = count_clicks(token, short_url_key)
             print('Количество кликов по ссылке: ', click_count)
+        else:
+            short_link = shorten_link(token, url)
+            print('Сокращенная ссылка: ', short_link)
 
-    except (requests.exceptions.RequestException, ValueError) as error:
+    except requests.exceptions.RequestException as error:
         print(f'Ошибка: {error}')
 
 
